47a5c696e1adf39007e1c2da1da1a1ec9448c176
kushal thakker <kushalthakker7@gmail.com>
2021-02-26 02:40:34 +0530

    Update Fp.py
    1st Step

--- Fp.py
+++ Fp.py
@@ -1,5 +1,12 @@
+####pip install alpha-vantage-pip install requests-pip install beautifulsoup4
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 from pandas_datareader import data, wb
 from datetime import date
+import matplotlib.pyplot as plt
+from alpha_vantage.timeseries import TimeSeries
+import requests
+from bs4 import BeautifulSoup
+
+###**my key = KZPT32AEEXXIV6Y2**
