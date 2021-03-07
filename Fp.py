3a972e8a9c583496bae73c38fe4d6b33d916c887
patel9530-spt <78999991+patel9530-spt@users.noreply.github.com>
2021-03-08 01:02:34 +0530

    Update Fp.py

--- Fp.py
+++ Fp.py
@@ -1,15 +1,24 @@
-####pip install alpha-vantage-pip install requests-pip install beautifulsoup4
-import numpy as np
 import pandas as pd
-import plotly.graph_objs as go
-from pandas_datareader import data, wb
-from datetime import date
 import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
-import requests
-from bs4 import BeautifulSoup
 
-confirmed_cases_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse _covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed _global.csv"
-deaths_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse _covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_glo bal.csv"
+api = 'C35O2SUDZPRYANJP'
 
-###**my AlphaVantage key = KZPT32AEEXXIV6Y2**
+deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+confirm = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+
+total_c = confirm.sum(axis = 0, skipna = True)
+totalc = total_c.to_list()
+
+total_d = deaths.sum(axis = 0, skipna = True)
+totald = total_d.to_list()
+
+timestamp = deaths.columns.to_list()
+del timestamp[0:4]
+del totalc[0:3]
+del totald[0:3]
+
+covid_cd = pd.DataFrame(list(zip(timestamp,totalc,totald)), columns = ['Timestamp', 'Total_Confirmed_Cases', 'Total_Deaths'])
+covid_cd['Total_Confirmed_Cases'] = covid_cd.Total_Confirmed_Cases.astype('int')
+covid_cd['Total_Deaths'] = covid_cd.Total_Deaths.astype('int')
+covid_cd['Timestamp'] = pd.to_datetime(covid_cd['Timestamp'])
